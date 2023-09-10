08410ba795881a7f589ba1786e7a63bb2ec5d59d
imranmahmood-mba <im6979@rit.edu>
2023-09-10 00:03:58 -0700

    added heapify function checks for max heap

--- chapter6/dijkstras_algorithm.py
+++ chapter6/dijkstras_algorithm.py
@@ -3,4 +3,4 @@ import heapq
 def dj_algorithm(start, end, graph):
     distances = {node: float('inf') for node in graph} # set the distances to 0
     distances[start] = 0
-    pass
+    priority_queue = [(0, start)]
